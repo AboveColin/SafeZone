2453a51e67d8bd2ed356e9f467733dd7e678d426
AboveColin <colindv2000@gmail.com>
2021-03-31 09:55:09 +0200

    Update main.py

--- ESP/main.py
+++ ESP/main.py
@@ -2,12 +2,7 @@ from machine import Pin
 import machine
 import read as read
 import ubinascii
-import online
-
-D_ID = ubinascii.hexlify(machine.unique_id()).decode('utf-8')
-if D_ID == "caca1300":
-    ledW = Pin(16, Pin.OUT)
-    ledW.value(1)
+import ws as WS
 
 
 def do_connect():
@@ -19,19 +14,15 @@ def do_connect():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     print('connecting to network...')
-    wlan.connect('***', '***')
+    wlan.connect('H369ABEE905', '9C43DECA7AAA')
     print('network config:', wlan.ifconfig())
     while not wlan.isconnected():
-        if D_ID == "caca1300":
-            ledW.value(1)
         machine.idle()  # save power while waiting
     if wlan.isconnected():
-        if D_ID == "caca1300":
-            ledW.value(0)
         print("ONLINE!")
-        read.send_online()
+        read.do_read()
 
 
 if __name__ == "__main__":
     do_connect()
-    read.do_read()
+
