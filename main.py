097ffcaccd9fb66af9553f36816cb519a59b7348
AboveColin <colindv2000@gmail.com>
2021-03-31 09:51:02 +0200

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-
-Def sup(nerds):
-  """Zonder docstring is niets compleet #DOD
-  """
-  if snap_er_niks_van == true;
-    return Depressie
-
-  assert sup(nerd) == Depressie ## Nooit de super belangrijke asserts vergeten!!
