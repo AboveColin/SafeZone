671984702a41b818a9f90f75ece297e7b6a18854
AboveColin <colindv2000@gmail.com>
2021-05-20 13:22:37 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,110 +1,65 @@
 #!/usr/bin/python
-from flask import Flask, render_template, request, jsonify
-import functions as Function
-import json
-import logging
-from models import log, log_type
-from logs import logger
-
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-
-online_list = []
-
+from flask import Flask, render_template, redirect, request, jsonify, request, flash, url_for
+from flask_login import login_user, login_required, logout_user, current_user
+from SafeZone import app
+from SafeZone.models import *
+from SafeZone.forms import *
 
 @app.route('/')
 def index():
-    return 'Active'
+    return render_template('home.html')
 
 
-@app.route('/online')
-def online():
-    request_data = json.loads(request.data)
-    reader_id = request_data[0]['esp_id']
-
-    ts = Function.get_current_timestamp()
-
-    readers = Function.DB.get_all_readers()
-    reader_list = []
-    for reader in readers:
-        r_id = reader[0]
-        reader_list.append(r_id)
-
-    if reader_id not in reader_list:
-        Function.DB.submit_reader_to_db(reader_id, ts)
-        reader_list.append(reader_id)
-
-    if reader_id not in online_list:
-        online_list.append(reader_id)
-
-    logger.info(log(reader_id).log_prefix_r() + ' ' + log_type.ONLINE + ' Reader {} is zojuist online gekomen.'.format(reader_id))
-    return ''
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("U bent succesvol uitgelogd")
+    return redirect(url_for("index"))
 
 
-@app.route('/online_check')
-def online_check():
-    request_data = json.loads(request.data)
-    reader_id = request_data[0]['esp_id']
+# Shows the user a login form
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    form = Loginform()
+    if form.validate_on_submit():
+         # Checks if the username is registered in the database
+        user = User.query.filter_by(username=form.username.data).first()
 
-    ts = Function.get_current_timestamp()
-
-    if reader_id not in online_list:
-        online_list.append(reader_id)
-
-    Function.DB.update_reader_last_seen(reader_id, ts)
-
-    logger.info(log(reader_id).log_prefix_r() + ' ' + log_type.PING + ' Ping')
-    return ''
-
-
-@app.route('/hit')
-def hit():
-    request_data = json.loads(request.data)
-
-    tag_id = "".join(list(request_data[0]['tag_id'])[-2])
-    reader_id = request_data[0]['esp_id']
-    ts = Function.get_current_timestamp()
-
-    if reader_id == '9c9c1fc88ea8':
-
-        # START / END
-
-        Function.DB.submit_data_to_db(reader_id, tag_id, ts)
-
-        person = Function.DB.get_person_from_T_id(tag_id)
+        if user is None or not user.check_password(form.password.data):
+            # If the username or password isn't recognised, the user gets flashed a message and will be redirected to the login page.
+            flash("Inloggen mislukt: gebruikersnaam/wachtwoord niet bekend.")
+            return redirect(url_for("login"))
         
-        logger.info(log(reader_id).log_prefix_r() + ' ' + log_type.READ + ' Data ontvangen van Tag: {}'.format(tag_id))
-        
-        if person is not False:    
-            logger.info(log(reader_id).log_prefix_r() + ' ' + log_type.INFO + ' Wandeling client: ' + str(person[0]) + ' gestart.')
-       
-        else:
-            logger.error(log(reader_id).log_prefix_r() + ' ' + log_type.INFO + ' Fout bij het ophalen van data bij gelezen tag')
-        
-        return reader_id
-    
-    else:
+        login_user(user)
+        flash("U bent succesvol ingelogd")
+        return redirect(url_for("index"))
 
-        Function.DB.submit_data_to_db(reader_id, tag_id, ts)
-        logger.info(log(reader_id).log_prefix_r() + ' ' + log_type.READ + ' Data ontvangen van Tag: {}'.format(tag_id))
-
-        return reader_id
+    return render_template("login.html", form=form)
 
 
-@app.route('/admin')
-def admin():
-    data = Function.get_data()
-    return render_template('admin.html',
-                           admin_reader_list=data[0][0],
-                           clients=data[0][1])
+# Shows the registration form
+@app.route("/register", methods=["GET", "POST"])
+def register():
 
+    # The registration form
+    form = Registratie()
+    if form.validate_on_submit():
 
-@app.route('/get_data')
-def return_data():
-    data = Function.get_data()
+        # Checks if the E-mail or Username is already in use
+        # If yes, the user gets flashed a message and will be send back to the registration page
+        user_username = User.query.filter_by(username=form.username.data).first()
+        if user_username:
+            flash("Gebruikersnaam al in gebruik, probeer in te loggen.")
+            return redirect(url_for("register"))
 
-    return jsonify(data[0])
+        user = User(username = form.username.data, password = form.password.data)
+        user_db.session.add(user)
+        user_db.session.commit()
+
+        flash("Bedankt voor het registreren")
+        return redirect(url_for("login"))
+    return render_template("register.html", form=form)
 
 
 if __name__ == '__main__':
